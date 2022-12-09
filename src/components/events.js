ba7eeeb11f9badccc829b9f7c9a2123f8ae8c049
LucioFurnari <luciofurnari99@gmail.com>
2022-12-09 19:12:03 -0300

    Create events module

---
+++ src/components/events.js
@@ -0,0 +1,18 @@
+import { createTask } from "./classes";
+import { taskArray } from "./classes";
+import { createTaskUi } from "./ui";
+
+export function addEvents(){
+    const testButton = document.querySelector(".testButton");
+    const main = document.querySelector(".todo-section");
+    const nameInput = document.querySelector(".nameInput")
+    const dateInput = document.querySelector(".dateInput")
+    const descriptionArea = document.querySelector(".descriptionArea")
+    testButton.addEventListener("click",(e) => {
+        e.preventDefault()
+        createTask(nameInput.value,dateInput.value,descriptionArea.value);
+        taskArray.forEach(elem => {
+            main.append(createTaskUi(elem))
+        })
+    })
+}
