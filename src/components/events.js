f9b852029fd583021aacdd6815910d8fa5392892
LucioFurnari <luciofurnari99@gmail.com>
2022-12-12 16:41:09 -0300

    Move dom selector of input to submit event, add dom selector for radio checkbox

--- src/components/events.js
+++ src/components/events.js
@@ -4,18 +4,29 @@ import { createTaskUi } from "./ui";
 
 export function addEvents(){
     const addTaskButton = document.querySelector(".addTaskButton");
-    const main = document.querySelector(".todo-section");
-    const nameInput = document.querySelector(".nameInput")
-    const dateInput = document.querySelector(".dateInput")
-    const descriptionArea = document.querySelector(".descriptionArea")
-    addTaskButton.addEventListener("click",(e) => {
-        e.preventDefault()
+    const main = document.querySelector(".tasks-container");
+    const taskForm = document.querySelector(".taskForm");
+    
+    addTaskButton.addEventListener("click",() => {
+        taskForm.classList.add("active")
+        
+    })
+
+    taskForm.addEventListener("submit",(e) => {
+        e.preventDefault();
+        const nameInput = document.querySelector(".nameInput");
+        const dateInput = document.querySelector(".dateInput");
+        const descriptionArea = document.querySelector(".descriptionArea");
+        const priorityInput = document.querySelector(".taskForm input[name = priority]:checked")
+        
+        console.log(priorityInput.value);
         while(main.firstChild){
             main.removeChild(main.firstChild)
         }
-        createTask(nameInput.value,dateInput.value,descriptionArea.value);
+        createTask(nameInput.value,dateInput.value,descriptionArea.value,priorityInput.value);
         taskArray.forEach(elem => {
             main.append(createTaskUi(elem))
         })
+        taskForm.classList.remove("active");
     })
 }
